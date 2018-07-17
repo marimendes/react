e8b11ee623e84735c8421ee3d06a51e1f18cbc02
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-17 10:26:09 -0300

    Exercicio 2 de componente

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Primeiro = props => (
+export default props => (
 	<h1>Primeiro Componente!</h1>
 )
 
-const Segundo = props => (
-	<h1>Segundo Componente!</h1>
-)
+export const Segundo = props => <h1>Segundo Componente!</h1>
 
-export { Primeiro, Segundo }
+//export { Primeiro, Segundo }
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Primeiro, Segundo } from './component'
+import Primeiro, { Segundo } from './component'
 
 ReactDOM.render(
 <div>
